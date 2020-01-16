809ca46c93fe50a032eec01e6e92bfde132f40cc
NKWOO <nkw9986@gmail.com>
2020-01-16 22:42:49 +0900

    자료형 사용법

--- dist/parctice.js
+++ dist/parctice.js
@@ -1,3 +1,17 @@
 "use strict";
-const message = 'hello world';
-console.log(message);
+let count = 0;
+count += 1;
+//count = "I am String!"; //Error 숫자형에 문자를 대입하려고 햇기에
+//Type '"I am String!"' is not assignable to type 'number'.ts(2322)
+const message = "hello world";
+const done = true;
+const numbers = [1, 2, 3];
+const messages = ['hello', 'world'];
+//messages.push(1); //문자열 배열에 숫자형을 대입하려고 햇기에!
+//Type '"green"' is not assignable to type '"red" | "orange" | "yellow"'.ts(2322)
+let mightBeUndefined = undefined;
+let nullableNumber = null;
+let color = 'red';
+color = 'yellow';
+//color = 'green'; //정해진 데이터에서 없는값을 대입하려고 햇기에!
+//Type '"green"' is not assignable to type '"red" | "orange" | "yellow"'.ts(2322)
--- src/parctice.ts
+++ src/parctice.ts
@@ -1,2 +1,23 @@
+let count = 0;
+count += 1;
+//count = "I am String!"; //Error 숫자형에 문자를 대입하려고 햇기에
+//Type '"I am String!"' is not assignable to type 'number'.ts(2322)
+
 const message: string = "hello world";
-console.log(message);
+
+const done: boolean = true;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ['hello', 'world']; 
+
+//messages.push(1); //문자열 배열에 숫자형을 대입하려고 햇기에!
+//Type '"green"' is not assignable to type '"red" | "orange" | "yellow"'.ts(2322)
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: number | null = null;
+
+let color: 'red' | 'orange' | 'yellow' = 'red';
+
+color = 'yellow';
+//color = 'green'; //정해진 데이터에서 없는값을 대입하려고 햇기에!
+//Type '"green"' is not assignable to type '"red" | "orange" | "yellow"'.ts(2322)
